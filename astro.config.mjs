30f5a0a0167fb3f185fe1b77fee7e7093c68ddce
Valentina Diaz <mariavaledb@hotmail.com>
2026-04-27 16:47:20 -0500

    Added GitHub Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://valdiba.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
